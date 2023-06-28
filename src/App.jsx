234c6eca5e04693c129b67e22c9785cf18023416
sadelto0130 <vsalaz00@live.com>
2023-06-28 02:59:59 -0300

    Proyecto Finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect} from 'react'
+import styled from '@emotion/styled'
+import ImagenCripto from './img/imagen-criptos.png'
+import Formulario from './components/Formulario'
+import Resultado from './components/Resultado'
+import Spinner from './components/Spinner'
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`
+const Imagen = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`
+
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #fff;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after{
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+`
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [monedas, setMonedas] = useState({})
+  const [cotizacion, setCotizacion] = useState({})
+  const [cargando, setCargando] = useState(false)
+
+  useEffect(()=>{
+    if(Object.keys(monedas).length > 0) {
+      const cotizarCritpo = async () => {
+        setCargando(true)
+        setCotizacion({})
+        
+        const {moneda, cripto} = monedas
+        const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${cripto}&tsyms=${moneda}`
+
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json()
+
+        setCotizacion(resultado.DISPLAY[cripto][moneda])
+        setCargando(false)
+      }
+      cotizarCritpo()
+    }
+  }, [monedas])
 
   return (
-    <>
+    <Contenedor>
+      <Imagen
+        src={ImagenCripto}
+        alt='Imagen cripto'
+      />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Heading>Cotiza Criptomonedas al Instante</Heading>
+        <Formulario
+          setMonedas = {setMonedas}
+        />
+        {cargando && <Spinner/>}
+        {cotizacion.PRICE && <Resultado cotizacion={cotizacion}/>}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </Contenedor>
   )
 }
 
---
+++ src/data/moneda.js
@@ -0,0 +1,10 @@
+const monedas = [
+  {id: 'USD', nombre: 'Dolar de Estados Unidos'},
+  {id: 'COP', nombre: 'Peso Colombiano'},
+  {id: 'ARS', nombre: 'Peso Argentino'},
+  {id: 'EUR', nombre: 'EURO'}
+]
+
+export {
+  monedas
+}
